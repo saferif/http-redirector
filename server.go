b337ee4cda70f271f6f7039d32a73f7128e01fe9
Insaf Latypov <saferif@gmail.com>
2015-02-24 08:55:08 +0300

    DefaultServeMux is not used any more.

--- server.go
+++ server.go
@@ -5,12 +5,11 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Host != "" {
 			http.Redirect(w, r, "http://www."+r.Host, http.StatusMovedPermanently)
 		} else {
 			http.Error(w, "Host header must be specified", http.StatusBadRequest)
 		}
-	})
-	http.ListenAndServe(":80", nil)
+	}))
 }
