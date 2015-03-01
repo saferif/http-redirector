9bdf5c531d46844a874e4058f064b0c973208b25
Insaf Latypov <saferif@gmail.com>
2015-03-02 01:14:35 +0300

    Request URI added to redirect location.

--- server.go
+++ server.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Host != "" {
-			http.Redirect(w, r, "http://www."+r.Host, http.StatusMovedPermanently)
+			http.Redirect(w, r, "http://www."+r.Host+r.RequestURI, http.StatusMovedPermanently)
 		} else {
 			http.Error(w, "Host header must be specified", http.StatusBadRequest)
 		}
